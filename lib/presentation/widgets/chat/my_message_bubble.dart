b8109aa276ca23260e39fed8fbf06e050d31111d
Juan Manuel Carranza <ikkicarranza@gmail.com>
2024-09-22 22:43:15 -0400

    Mensajes de mi

--- lib/presentation/widgets/chat/my_message_bubble.dart
+++ lib/presentation/widgets/chat/my_message_bubble.dart
@@ -5,6 +5,23 @@ class MyMessageBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    final colors = Theme.of(context).colorScheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Container(
+          decoration:  BoxDecoration(
+            color: colors.primary,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Text("Consequat ipsum do Lorem ullamco magna.", style: TextStyle( color: Colors.white),),
+          ),
+        ),
+        const SizedBox(height: 10,)
+      ],
+    );
   }
 }
