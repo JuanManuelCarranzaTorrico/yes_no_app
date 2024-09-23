be6cedf04ade53425973e1492f6252111f06e56f
Juan Manuel Carranza <ikkicarranza@gmail.com>
2024-09-22 22:21:08 -0400

    First Commit

---
+++ lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding:  EdgeInsets.all(4.0),
+          child:  CircleAvatar(
+            backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8QC2pkdLxW24wkO5gejjhRfduqucTlhrdsQ&s'),
+          ),
+        ),
+        title: const Text("Batman 🦇"),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric( horizontal: 10 ),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                return Text('Indice: $index');
+              },)
+              ),
+            Text("Mundo")
+          ]
+        ),
+      ),
+    );
+  }
+}
