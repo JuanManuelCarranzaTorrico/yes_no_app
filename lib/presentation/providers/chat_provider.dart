7a603db96702db5da5336dca46b1a874295e3e73
Juan Manuel Carranza <ikkicarranza@gmail.com>
2024-09-28 11:12:23 -0400

    Using chatPrivider to use show generic messages

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier{
-  List<Message> messages = [
+  List<Message> messageList = [
     Message(text: "Hola Gotica", fromWho: FromWho.me),
     Message(text: "Ya Volviste?", fromWho: FromWho.me)
   ];
