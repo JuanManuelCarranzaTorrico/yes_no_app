6e75e197bae503466c2a7c98b9b3000f0ac14f71
Juan Manuel Carranza <ikkicarranza@gmail.com>
2024-09-28 10:58:35 -0400

    Add provider to the project

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier{
+  List<Message> messages = [
+    Message(text: "Hola Gotica", fromWho: FromWho.me),
+    Message(text: "Ya Volviste?", fromWho: FromWho.me)
+  ];
+
+  Future<void> sendMessage (String text ) async {
+    //todo implementar metodo
+  }
+}
